c1fb33a7e6a75491ec1b4d0aa1e79b5cde9031e0
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2024-11-08 21:08:41 +0400

    GH-000 Add client message stream

---
+++ modules/server/src/services/MessageProcessorService.ts
@@ -0,0 +1,10 @@
+import * as Context from "effect/Context";
+
+export class MessageHandlingService extends Context.Tag(
+  "MessageHandlingService")<
+  MessageHandlingService,
+  {
+
+  }
+>() {
+}
