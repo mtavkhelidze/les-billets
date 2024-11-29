4d12a8d79e2a3d04ea3406c4aa7534a6c888c966
Misha Tavkhelidze <misha.tavkhelidze@gmail.com>
2024-11-29 11:05:29 +0400

    GH-000 Fix layout

--- packages/client/src/blocks/Container.tsx
+++ packages/client/src/blocks/Container.tsx
@@ -3,7 +3,7 @@ import React, { type PropsWithChildren } from "react";
 
 export const Container: React.FC<PropsWithChildren> = ({ children }) => {
   return <div
-    className="w-full md:w-2/3 lg:w-1/2 bg-gray-50 px-4 py-2 4 m-auto flex flex-col min-h-screen"
+    className="w-full md:w-4/5 bg-gray-50 px-4 py-2 4 m-auto flex flex-col min-h-screen"
   >
     <Header />
     <div className="container flex-1 flex justify-center pt-2">
